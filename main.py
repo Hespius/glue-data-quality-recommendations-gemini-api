1605555be981750e7c20f3af8a4867d22ff3272e
Vinicius <especoto.vini@gmail.com>
2025-01-16 23:09:18 -0300

    feat: enhance data quality rules generation with AWS Glue instructions

--- main.py
+++ main.py
@@ -15,16 +15,36 @@ generation_config = {
   "response_mime_type": "text/plain",
 }
 
+instructions = """
+Act as a data engineer specializing in data quality. Your primary tool is AWS Glue, and you use AWS Glue Data Quality to monitor the data quality of your tables.
+
+Respond only with the data quality rules I can apply to the table following the syntax of AWS Glue Data Quality's DQDL, according to the example below:
+
+Rules = [
+  'IsComplete "COLUMN_NAME"',
+  'IsUnique "COLUMN_NAME"'
+]
+"""
+
 model = genai.GenerativeModel(
   model_name="gemini-2.0-flash-exp",
   generation_config=generation_config,
+  system_instruction=instructions
 )
 
-chat_session = model.start_chat(
-  history=[
-  ]
-)
+prompt = """
+I have a table with the following attributes:
 
-response = chat_session.send_message("Você conhece o AWS Glue Data Quality? Pode me explicar como funciona?")
+id: integer
+nome: string
+idade: integer
+email: string
+data_cadastro: string
+
+I would like to know what data quality rules I can apply to the table.
+
+"""
+
+response = model.generate_content(prompt)
 
 print(response.text)

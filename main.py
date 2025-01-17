1a96e421697b86930d28e7ae2d6bbe7e6e41c3f0
Vinicius <especoto.vini@gmail.com>
2025-01-16 22:26:25 -0300

    feat: add initial implementation for Gemini AI chat model and dependencies

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-2.0-flash-exp",
+  generation_config=generation_config,
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
+response = chat_session.send_message("Me explique as diferenças entre os modelos Gemini 2.0 Flash Experimental e o Gemini 2.0 Flash Thinking Experimental")
+
+print(response.text)

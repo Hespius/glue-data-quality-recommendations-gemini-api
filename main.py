94e12b4431e06a4d4e4e043b22beb2c81b9ec2a2
Vinicius <especoto.vini@gmail.com>
2025-01-17 11:22:38 -0300

    feat: update Dockerfile and main.py for FastAPI integration and data quality rules generation

--- main.py
+++ main.py
@@ -1,50 +1,62 @@
 import os
-from dotenv import load_dotenv
+import re
+import json
 
+from dotenv import load_dotenv
+from fastapi import FastAPI
 import google.generativeai as genai
 
 load_dotenv()
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+app = FastAPI()
 
-generation_config = {
-  "temperature": 1,
-  "top_p": 0.95,
-  "top_k": 40,
-  "max_output_tokens": 8192,
-  "response_mime_type": "text/plain",
-}
+@app.get("/glue-data-quality-recommendations-rules")
+def read_root(model_name: str = "gemini-2.0-flash-exp"):
+  genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    
 
-instructions = """
-Act as a data engineer specializing in data quality. Your primary tool is AWS Glue, and you use AWS Glue Data Quality to monitor the data quality of your tables.
+  generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 40,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+  }
 
-Respond only with the data quality rules I can apply to the table following the syntax of AWS Glue Data Quality's DQDL, according to the example below:
+  instructions = """
+  Act as a data engineer specializing in data quality. Your primary tool is AWS Glue, and you use AWS Glue Data Quality to monitor the data quality of your tables.
 
-Rules = [
-  'IsComplete "COLUMN_NAME"',
-  'IsUnique "COLUMN_NAME"'
-]
-"""
+  Respond only with the data quality rules I can apply to the table following the syntax of AWS Glue Data Quality's DQDL, according to the example below:
 
-model = genai.GenerativeModel(
-  model_name="gemini-2.0-flash-exp",
-  generation_config=generation_config,
-  system_instruction=instructions
-)
+  Rules = [
+    'IsComplete "COLUMN_NAME"',
+    'IsUnique "COLUMN_NAME"'
+  ]
+  """
 
-prompt = """
-I have a table with the following attributes:
+  model = genai.GenerativeModel(
+    model_name=model_name,
+    generation_config=generation_config,
+    system_instruction=instructions
+  )
 
-id: integer
-nome: string
-idade: integer
-email: string
-data_cadastro: string
+  prompt = """
+  I have a table with the following attributes:
 
-I would like to know what data quality rules I can apply to the table.
+  id: integer
+  nome: string
+  idade: integer
+  email: string
+  data_cadastro: string
 
-"""
+  I would like to know what data quality rules I can apply to the table.
 
-response = model.generate_content(prompt)
+  """
 
-print(response.text)
+  response = model.generate_content(prompt)
+
+  # Extract rules from the response text
+  rules_text = re.search(r'Rules = \[(.*?)\]', response.text, re.DOTALL).group(1)
+  rules_list = [rule.strip().strip("'") for rule in rules_text.split(',\n')]
+
+  return {"rules": rules_list}
